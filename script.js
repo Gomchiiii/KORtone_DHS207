4aa7fbdde13a8a14a9b51cc287150fc98caafb3c
Gomchiiii <161728639+Gomchiiii@users.noreply.github.com>
2024-05-19 21:38:13 +0900

    Update script.js remove darkmode in if notive

--- script.js
+++ script.js
@@ -205,13 +205,10 @@ function isSamsungBrowser() {
     return userAgent.includes('samsungbrowser');
 }
 
-function isDarkMode() {
-    return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-}
 
 function showNotice() {
     const notice = document.querySelector('.notice');
-    if (isSamsungBrowser() && isDarkMode()) {
+    if (isSamsungBrowser()) {
         notice.style.display = 'block';
     } 
 }
